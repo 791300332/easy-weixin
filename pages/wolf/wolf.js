467362d359af88a4bd8b0f72a3cb40607e1e5111
sungaoxiang <sungaoxiang@xkeshi.com>
2018-10-27 21:02:47 +0800

    备份

--- pages/wolf/wolf.js
+++ pages/wolf/wolf.js
@@ -1,18 +1,21 @@
 // pages/wolf/wolf.js
+var url = 'wss://www.jiachao.online/websocket';
+var socketOpen = false;
 var common = require('../../utils/api.js')
 Page({
   data: {
     userInfo:{},
     list:[],
+    index:0,
+    fg:null,
     room:null,
     needAdd:[],
     hasJoin:false,
     modalCreate:true,
     modalJoin:true,
+    modalFg:true,
     joinRoomId:null,
-    listen:false,
-    timeoutNum:null,
-    isHide:false,
+    socketTask:null,
     createInfo:{
       roomId:'',
       identifyDTO:[
@@ -29,56 +32,59 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    common.post('/user/miniapp/getInfo', {}).then(res => {
+      this.setData({
+        userInfo: res.result
+      })
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    common.post('/user/miniapp/getInfo',{}).then(res =>{
-      this.setData({
-        userInfo:res.result
-      })
-    })
+    setTimeout(this.listen,5000,this);
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.setData({
-      isHide:false
-    })
-    this.updateRoom(this);
+    var that = this;
+    if(this.data.userInfo == null) {
+      common.post('/user/miniapp/getInfo', {}).then(res => {
+        that.data.userInfo = res.result;
+        that.updateRoom(that);
+      })
+    } else {
+      this.updateRoom(that);
+    }
   },
 
   updateRoom:function(obj){
-    if(!obj.data.isHide) {
-      let that = this;
-      common.post('/wolf/kill/hasJoin', {}).then(res => {
-        let isJoin = res.result;
-        if (!isJoin) {
-          wx.setNavigationBarTitle({
-            title: '杀狼人',
+    console.log(obj);
+    let that = obj;
+    common.post('/wolf/kill/hasJoin', {}).then(res => {
+      let isJoin = res.result;
+      var userInfo = that.data.userInfo;
+      if (!isJoin) {
+        wx.setNavigationBarTitle({
+          title: '杀狼人',
+        })
+        common.post('/wolf/kill/list', {}).then(res => {
+          that.setData({
+            list: res.result,
+            hasJoin: isJoin,
+            userInfo: userInfo,
+            modalJoin:true,
+            modalCreate:true,
+            modalFg:true
           })
-          var tempNum = that.data.timeoutNum;
-          if (tempNum != null) {
-            clearTimeout(tempNum);
-          }
-          common.post('/wolf/kill/list', {}).then(res => {
-            that.setData({
-              list: res.result,
-              hasJoin: isJoin,
-              listen: false,
-              timeoutNum: null
-            })
-          })
-        } else {
-          that.updateRoomInfo(that);
-        }
-      })
-    }
+        })
+      } else {
+        that.updateRoomInfo(that);
+      }
+    })
   },
 
   updateRoomInfo:function(that) {
@@ -86,20 +92,92 @@ Page({
       wx.setNavigationBarTitle({
         title: '房间号:' + res.result.roomId,
       })
-      var needNum = res.result.needTotalNum - res.result.hasNum;
-      var temp = [];
-      for (var i = 0; i < needNum; i++) {
-        temp.push(i);
-      }
-      var tempNum = setTimeout(that.updateRoom, 5000,that);
-      that.setData({
-        room: res.result,
-        hasJoin: true,
-        needAdd: temp,
-        listen: true,
-        timeoutNum: tempNum
-      })
+      that.putRoomInfo(that,res.result);
     });
+  },
+
+  putRoomInfo:function(that,res) {
+    var needNum = res.needTotalNum - res.hasNum;
+    var temp = [];
+    for (var i = 0; i < needNum; i++) {
+      temp.push(i);
+    };
+    if (that.data.socketTask == null) {
+      var socketTask = that.webSocket();
+      that.data.socketTask = socketTask;
+    }
+    that.setData({
+      room: res,
+      hasJoin: true,
+      needAdd: temp,
+      userInfo: that.data.userInfo,
+      socketTask: that.data.socketTask
+    });
+  },
+  webSocket: function () {
+    let that = this;
+    // 创建Socket
+    var socketTask = wx.connectSocket({
+      url: url,
+      header: {
+        'content-type': 'application/json'
+      },
+      method: 'post',
+      success: function (res) {
+        console.log('WebSocket连接创建', res)
+      },
+      fail: function (err) {
+        wx.showToast({
+          title: '网络异常！',
+        })
+        console.log(err)
+      },
+    });
+    socketTask.onOpen(res => {
+      socketOpen = true;
+      console.log('监听 WebSocket 连接打开事件。', res);
+    });
+    socketTask.onClose(onClose => {
+      console.log('监听 WebSocket 连接关闭事件。', onClose)
+      socketOpen = false;
+    });
+    socketTask.onError(onError => {
+      console.log('监听 WebSocket 错误。错误信息', onError)
+      socketOpen = false
+    });
+    socketTask.onMessage(onMessage => {
+      console.log('监听WebSocket接受到服务器的消息事件。服务器返回的消息', JSON.parse(onMessage.data))
+      var result = JSON.parse(onMessage.data)
+      var status = result.status;
+      if (status == 'none') {
+
+      } else if (status == 'delete' && this.hasJoin) {
+        this.onShow();
+      } else if (status == 'update') {
+        that.putRoomInfo(that, JSON.parse(result.room));
+      }
+    });
+    return socketTask;
+  },
+
+  listen:function(obj) {
+    let task = this.data.socketTask;
+    if (socketOpen && this.data.room != null) {
+      var str = '{"ownerId":"' + this.data.userInfo.xCardUserId + '","updateVersion":"' + this.data.room.updateVersion + '"}';
+      console.log(str);
+      task.send({
+        data:str,
+        success:function(e) {
+          console.log('发送成功:',e);
+        }
+      })
+    } else if(!socketOpen){
+      task = this.webSocket();
+      this.setData({
+        socketTask:task
+      })
+    }
+    setTimeout(this.listen,1000,this);
   },
 
   btnJoin:function(e) {
@@ -135,6 +213,11 @@ Page({
   btnModalJoin:function(e) {
     this.setData({
       modalJoin:false
+    })
+  },
+  btnModalFg:function(e) {
+    this.setData({
+      modalFg:false
     })
   },
   createInputChange:function(e) {
@@ -191,6 +274,11 @@ Page({
     }
   },
 
+  confirmFg : function(e) {
+    this.setData({
+      modalFg:true
+    })
+  },
   cancelJoin:function(e) {
     let that = this;
     that.setData({
@@ -205,35 +293,71 @@ Page({
     });
   },
 
+  cancelFg: function(e) {
+    this.setData({
+      modalFg:true
+    })
+  },
+
+  pickerChange:function(e) {
+    var index = e.detail.value;
+    var selectUser = this.data.room.users[index];
+    this.setData({
+      fg:selectUser,
+      index:index,
+      modalFg:true
+    })
+  },
+
   delUser:function(e) {
     var that = this;
     var id = e.target.id;
-    wx.showModal({
-      title: '踢人',
-      content: '你确定要删除他吗',
-      success:function(res) {
-        if(res.confirm) {
-          common.post('/wolf/kill/del', { xCardUserId: id }).then(res => {
-            that.onShow();
-          })
+    console.log(that);
+    if(this.data.room.status == '创建') {
+      wx.showModal({
+        title: '踢人',
+        content: '你确定要删除他吗',
+        success: function (res) {
+          if (res.confirm) {
+            common.post('/wolf/kill/del', { xCardUserId: id }).then(res => {
+              that.onShow();
+            })
+          }
         }
-      }
-    })
+      })
+    } else {
+      wx.showModal({
+        title: '踢人',
+        content: '游戏已经开始了，不能踢人哦',
+        showCancel:false
+      });
+    }
+    
   },
 
   startGame: function(){
     let that = this;
-    common.post('/wolf/kill/startGame',{}).then(res =>{
-      that.updateRoom();
+    if(this.data.fg == null) {
+      wx.showToast({
+        title: '请指定法官',
+        icon:'none'
+      })
+    } else {
+      common.post('/wolf/kill/startGame', this.data.fg).then(res => {
+
+      })
+    }
+  },
+
+  stopGame:function(){
+    common.post('/wolf/kill/stopGame', {}).then(res => {
+
     })
   },
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    this.setData({
-      isHide:true
-    })
   },
 
   /**
